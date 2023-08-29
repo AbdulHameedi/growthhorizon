b1a2ec335a9acefd597181deee62c45db1b264c0
AbdulHameedi <sanilinity507@gmail.com>
2023-08-29 22:09:19 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data(){
+    return{
+
+    }
+  }
+})
+</script>
+<template>
+  <div class="text-red-500"> 
+    This is home
+  </div>
+</template>
