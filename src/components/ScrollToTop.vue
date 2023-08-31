c67c3222d0e5c81eee8d6738151971717e0ff53b
AbdulHameedi <sanilinity507@gmail.com>
2023-08-31 01:02:31 +0100

    'footer'

---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+  data(){
+    return{
+
+    }
+  }
+})
+</script>
+<template>
+    <div class="w-fit bg-[#2E3BA6] rounded-md absolute bottom-[3rem] right-[2rem] text-white p-3 max-[425px]:p-2 max-[425px]:right-[1rem]">
+        <svg class="hover:translate-y-[-1px]" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m12 10.8l-3.9 3.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275L12 10.8Z"/></svg>
+    </div>
+</template>
