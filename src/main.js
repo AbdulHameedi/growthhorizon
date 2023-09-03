fe96d79be65a1778149030d3137d39ce477dcd49
AbdulrahmanLuqman <fal528@dervac.com>
2023-09-03 06:48:52 +0100

    added aos

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const app = createApp(App)
 
+app.use(AOS.init())
 app.use(createPinia())
 app.use(router)
 
