c2ae3d81e0af28ed54507eeaf9efd9a8644e11b2
AbdulrahmanLuqman <fal528@dervac.com>
2023-09-05 15:30:46 +0100

    done with herosection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -20,6 +20,80 @@
                 <button class=" lg:opacity-50 opacity-75 hover:opacity-100 transition-opacity duration-700 bg-white rounded-[50%] p-2" @click="nextSlide"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#0D0D3F" d="M16.15 13H5q-.425 0-.713-.288T4 12q0-.425.288-.713T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.313t.712.288L19.3 11.3q.15.15.213.325t.062.375q0 .2-.063.375t-.212.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7L16.15 13Z"/></svg></button>
             </div>
         </div>
+
+        <div class="grid lg:grid-cols-3 md:grid-cols-2 w-fit mx-auto gap-6">
+          <div class="about-summary cursor-pointer bg-[url(src/assets/about-summary-1.webp)] bg-center bg-cover h-[260px] w-[370px] mt-[-30px] mx-auto relative overflow-hidden rounded-lg">
+
+
+
+          <div class="green-ball z-[300] absolute bg-[#28AA9A] opacity-[0.9] w-[200px] h-[200px] bottom-[-120px] left-[-90px] rounded-[50%] transition-all duration-700">
+          
+          </div>
+          <div class="relative blue-bg transition-all duration-700 left-0 right-0 w-[300px] bottom-[-100px] h-[300px] mx-auto w-full">
+            <div class="absolute blue-bg transition-all duration-700 bg-[#3E359A] z-[100] opacity-[0.8] h-full  left-0 right-0 mx-auto rounded-[50%]"></div>
+            <!-- arrow -->
+            <div class="arrow bg-white rounded-[50%] h-fit absolute right-8 z-[150] p-4 opacity-75"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#0D0D3F" d="M16.15 13H5q-.425 0-.713-.288T4 12q0-.425.288-.713T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.313t.712.288L19.3 11.3q.15.15.213.325t.062.375q0 .2-.063.375t-.212.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7L16.15 13Z"/></svg></div>
+            <div class="texts absolute space-y-4 top-[70px] left-0 right-0 mx-auto w-[200px] text-center z-[500]">
+              <div class="flex items-center gap-6">
+                <svg class="summary-text hidden" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M8 2H1l8.26 11.014L1.45 22H4.1l6.388-7.349L16 22h7l-8.608-11.478L21.8 2h-2.65l-5.986 6.886L8 2Zm9 18L5 4h2l12 16h-2Z"/></svg>
+                <p class="text-white text-xl font-bold">Boost your page</p>
+              </div>
+              <p class="summary-text text-gray-300 hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, accusamus.</p>
+            </div>
+          </div>
+          <!-- <div class="blue-bg bg-[#3E359A] mx-auto opacity-[0.8] w-[300px] h-[300px] rounded-[50%]">
+          
+          </div> -->
+          </div>
+
+          <div class="about-summary cursor-pointer bg-[url(src/assets/about-summary-2.jpeg)] bg-center bg-cover h-[260px] w-[370px] md:mt-[-30px] mx-auto relative overflow-hidden rounded-lg">
+
+
+
+          <div class="green-ball z-[300] absolute bg-[#28AA9A] opacity-[0.9] w-[200px] h-[200px] bottom-[-120px] left-[-90px] rounded-[50%] transition-all duration-700">
+          
+          </div>
+          <div class="relative blue-bg transition-all duration-700 left-0 right-0 w-[300px] bottom-[-100px] h-[300px] mx-auto w-full">
+            <div class="absolute blue-bg transition-all duration-700 bg-[#3E359A] z-[100] opacity-[0.8] h-full  left-0 right-0 mx-auto rounded-[50%]"></div>
+            <!-- arrow -->
+            <div class="arrow bg-white rounded-[50%] h-fit absolute right-8 z-[150] p-4 opacity-75"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#0D0D3F" d="M16.15 13H5q-.425 0-.713-.288T4 12q0-.425.288-.713T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.313t.712.288L19.3 11.3q.15.15.213.325t.062.375q0 .2-.063.375t-.212.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7L16.15 13Z"/></svg></div>
+            <div class="texts absolute space-y-4 top-[70px] left-0 right-0 mx-auto w-[200px] text-center z-[500]">
+              <div class="flex items-center gap-6">
+                <svg class="summary-text hidden" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M8 2H1l8.26 11.014L1.45 22H4.1l6.388-7.349L16 22h7l-8.608-11.478L21.8 2h-2.65l-5.986 6.886L8 2Zm9 18L5 4h2l12 16h-2Z"/></svg>
+                <p class="text-white text-xl font-bold">Save your Time and money</p>
+              </div>
+              <p class="summary-text text-gray-300 hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, accusamus.</p>
+            </div>
+          </div>
+          <!-- <div class="blue-bg bg-[#3E359A] mx-auto opacity-[0.8] w-[300px] h-[300px] rounded-[50%]">
+          
+          </div> -->
+          </div>
+
+          <div class="about-summary cursor-pointer bg-[url(src/assets/about-summary-3.webp)] bg-center bg-cover h-[260px] w-[370px] lg:mt-[-30px] mx-auto relative overflow-hidden rounded-lg">
+
+
+
+          <div class="green-ball z-[300] absolute bg-[#28AA9A] opacity-[0.9] w-[200px] h-[200px] bottom-[-120px] left-[-90px] rounded-[50%] transition-all duration-700">
+          
+          </div>
+          <div class="relative blue-bg transition-all duration-700 left-0 right-0 w-[300px] bottom-[-100px] h-[300px] mx-auto w-full">
+            <div class="absolute blue-bg transition-all duration-700 bg-[#3E359A] z-[100] opacity-[0.8] h-full  left-0 right-0 mx-auto rounded-[50%]"></div>
+            <!-- arrow -->
+            <div class="arrow bg-white rounded-[50%] h-fit absolute right-8 z-[150] p-4 opacity-75"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#0D0D3F" d="M16.15 13H5q-.425 0-.713-.288T4 12q0-.425.288-.713T5 11h11.15L13.3 8.15q-.3-.3-.288-.7t.288-.7q.3-.3.713-.313t.712.288L19.3 11.3q.15.15.213.325t.062.375q0 .2-.063.375t-.212.325l-4.575 4.575q-.3.3-.712.288t-.713-.313q-.275-.3-.288-.7t.288-.7L16.15 13Z"/></svg></div>
+            <div class="texts absolute space-y-4 top-[70px] left-0 right-0 mx-auto w-[200px] text-center z-[500]">
+              <div class="flex items-center gap-6">
+                <svg class="summary-text hidden" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="white" d="M8 2H1l8.26 11.014L1.45 22H4.1l6.388-7.349L16 22h7l-8.608-11.478L21.8 2h-2.65l-5.986 6.886L8 2Zm9 18L5 4h2l12 16h-2Z"/></svg>
+                <p class="text-white text-xl font-bold">Earn from home</p>
+              </div>
+              <p class="summary-text text-gray-300 hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, accusamus.</p>
+            </div>
+          </div>
+          <!-- <div class="blue-bg bg-[#3E359A] mx-auto opacity-[0.8] w-[300px] h-[300px] rounded-[50%]">
+          
+          </div> -->
+          </div>
+        </div>
     </div>
 </template>
 
@@ -81,5 +155,25 @@
   transition: all 3s ease;
 
 }
-
+.about-summary:hover .green-ball{
+  bottom: -200px;
+  left: -200px;
+  transition: all 400ms ease;
+}
+.about-summary:hover .blue-bg{
+  width: 100%;
+  height: 100%;
+  bottom: 0;
+  border-radius: 0px;
+  transition: all 400ms ease-in-out;
+}
+.about-summary:hover .summary-text{
+  display: block;
+}
+.about-summary:hover .arrow{
+  display: none;
+}
+.about-summary:hover .texts{
+  top: 20px;
+}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -34,7 +34,7 @@
                 <p class="text-[#0D0D3F] text-xl font-bold">GrowthHorizon</p>
             </div>
     
-            <nav class="lg:flex lg:static fixed top-0 lg:bg-transparent bg-[#0D0D3F] lg:w-fit w-[90%] lg:h-fit h-screen lg:p-0 md:p-16 p-6 gap-6 space-y-6 lg:overflow-hidden overflow-y-auto z-[999] transition-right duration-700" :class="!showNavs ? 'right-[-1500px]' : 'right-0'">
+            <nav class="lg:flex items-center lg:static fixed top-0 lg:bg-transparent bg-[#0D0D3F] lg:w-fit w-[90%] lg:h-full h-screen lg:p-0 md:p-16 p-6 gap-6 space-y-6 lg:overflow-hidden overflow-y-auto z-[999] transition-right duration-700" :class="!showNavs ? 'right-[-1500px]' : 'right-0'">
                 <div @click="showNavs = false" class="lg:hidden block absolute top-6 right-6 w-fit cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="white" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"/></svg>
                 </div>
