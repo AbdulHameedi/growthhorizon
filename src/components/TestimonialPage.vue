9a397570f68613e5ec9e61c4229b70da4289e545
AbdulHameedi <sanilinity507@gmail.com>
2023-09-23 10:57:20 +0100

    testimonial-page

--- src/components/TestimonialPage.vue
+++ src/components/TestimonialPage.vue
@@ -8,10 +8,10 @@ export default defineComponent({
     components: {
         TestimonialCard,
         RegisterFooter,
-        Footer
+        Footer,
     }
 })
-</script>
+</script>   
 <template>
     <section>
         <div class="bg-[url('../assets/bg-section.jpg')] bg-center">
@@ -24,12 +24,10 @@ export default defineComponent({
                 </div>
             </div>
             <div class="px-[5.5rem] max-[1312px]:px-[1rem] grid grid-cols-3 py-20 gap-4 max-[880px]:grid-cols-2 max-[636px]:grid-cols-1" data-aos="fade-up" data-aos-duration="800" data-aos-offset="200"  data-aos-easing="ease-in-out">
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
+                <TestimonialCard name="Taiwo Balkis" job="Self Employed" testimony="Growth Horizon provides a unique opportunity for businesses to connect with a highly engaged user base. It's an effective and cost-efficient way to boost brand awareness and conversions."/>
+                <TestimonialCard name="Hammed Sanni" job="Student" testimony="Finally, a platform that rewards me for being an active social media user! Growth Horizon has allowed me to turn my online presence into a source of income."/>
+                <TestimonialCard name="Elevated Excellence Elementary" job="Youtube Channel" testimony="We've tried many advertising platforms, but Growth Horizon stands out. The quality of engagement is remarkable, and the ability to set specific campaign goals ensures we get the results we want."/>
+                <TestimonialCard name="Fawaz Makinde" job="Student" testimony="I was skeptical at first, but Growth Horizon is the real deal. I've earned a substantial amount just by sharing ads. This platform has changed the game for me."/>
             </div>
         </div>
         <RegisterFooter />
