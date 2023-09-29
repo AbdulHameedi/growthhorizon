52a16111e936c401a5373f572bfdcc548ab592b0
AbdulHameedi <sanilinity507@gmail.com>
2023-09-29 17:28:41 +0100

    price

--- src/components/Process.vue
+++ src/components/Process.vue
@@ -89,7 +89,7 @@ export default defineComponent({
                         </div>
                         <div class="flex gap-3 items-start">
                             <p class="w-fit bg-green-300 text-[#28B79A] p-2 rounded-[50%]"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m4 24l5-5l10 10L39 9l5 5l-25 25L4 24Z" clip-rule="evenodd"/></svg></p>
-                            <p class="font-semibold">Refer an advertiser and get N500.00 extra</p>
+                            <p class="font-semibold">Refer an advertiser and get N200.00 extra</p>
                         </div>
                         <div class="flex gap-3 items-start">
                             <p class="w-fit bg-green-300 text-[#28B79A] p-2 rounded-[50%]"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 48 48"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m4 24l5-5l10 10L39 9l5 5l-25 25L4 24Z" clip-rule="evenodd"/></svg></p>
